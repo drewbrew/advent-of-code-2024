98f32aabd3c60fa24838c27fd7983aafb58396ca
Drew Winstel <drew@hsv.beer>
2024-12-16 08:00:47 -0600

    Day 16: fun with mazes

--- day16.py
+++ day16.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 from pathlib import Path
 
 import networkx
@@ -19,13 +20,14 @@ TEST_INPUT = """###############
 ###############"""
 
 
-def display_grid(graph: networkx.Graph, start: tuple[int, int], end: tuple[int, int]):
-    nodes = sorted(graph.nodes)
+def display_grid(graph: networkx.DiGraph, start: tuple[int, int], end: tuple[int, int]):
+    nodes = sorted((x, y) for (x, y, _, _) in graph.nodes)
     min_x, min_y = nodes[0]
     max_x, max_y = nodes[-1]
+    node_set = set(nodes)
     for y in range(min_y - 1, max_y + 2):
         for x in range(min_x - 1, max_x + 2):
-            if (x, y) not in graph.nodes:
+            if (x, y) not in node_set:
                 print("#", end="")
             else:
                 if (x, y) == start:
@@ -37,8 +39,10 @@ def display_grid(graph: networkx.Graph, start: tuple[int, int], end: tuple[int, 
         print("")
 
 
-def parse_input(puzzle: str) -> tuple[networkx.Graph, tuple[int, int], tuple[int, int]]:
-    graph = networkx.Graph()
+def parse_input(
+    puzzle: str,
+) -> tuple[networkx.DiGraph, tuple[int, int], tuple[int, int]]:
+    graph = networkx.DiGraph()
     lines = puzzle.splitlines()
     start = ()
     end = ()
@@ -48,16 +52,24 @@ def parse_input(puzzle: str) -> tuple[networkx.Graph, tuple[int, int], tuple[int
                 start = (x, y)
             if char == "E":
                 end = (x, y)
+                # don't add edges leaving this node
+                continue
             if char == "#":
                 continue
-            for dx, dy in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                if dx >= 0 and dy >= 0:
+            # add edges representing each possible turn
+            for (dx1, dy1), (dx2, dy2) in permutations(
+                [(1, 0), (0, 1), (-1, 0), (0, -1)], 2
+            ):
+                graph.add_edge((x, y, dx1, dy1), (x, y, dx2, dy2), cost=1000)
+            for x1, y1 in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                if x1 >= 0 and y1 >= 0:
+                    dx = x1 - x
+                    dy = y1 - y
                     try:
-                        if lines[dy][dx] != "#":
-                            graph.add_edge((x, y), (dx, dy))
+                        if lines[y1][x1] != "#":
+                            graph.add_edge((x, y, dx, dy), (x1, y1, dx, dy), cost=1)
                     except IndexError:
                         continue
-    display_grid(graph, start, end)
     return graph, start, end
 
 
@@ -76,19 +88,52 @@ def path_score(path: list[tuple[int, int]], bearing: tuple[int, int] = (1, 0)):
     return score
 
 
+def cost(start: tuple[int, int, int, int], end: tuple[int, int, int, int], attrs):
+    return attrs["cost"]
+
+
 def part_one(puzzle: str) -> int:
     graph, start, end = parse_input(puzzle)
-    paths = networkx.all_simple_paths(graph, start, end)
-    paths_found = 0
+    display_grid(graph, start, end)
+    x_start, y_start = start
+    x_end, y_end = end
     best_score = 10000000000000000000000000000
-    for path in paths:
-        paths_found += 1
-        score = path_score(path)
-        if score < best_score:
-            print(f"found new best {score} after path number {paths_found}!", end="\r")
+    for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        try:
+            score = networkx.shortest_path_length(
+                graph, (x_start, y_start, 1, 0), (x_end, y_end, dx, dy), weight=cost
+            )
+        except (networkx.exception.NodeNotFound, networkx.exception.NetworkXNoPath):
+            print(f"no way to enter the end using direction {dx}, {dy}")
+        else:
+            if score < best_score:
+                print("new winner", score)
             best_score = score
-    print("")
     return best_score
+
+
+def part_two(puzzle: str, part_one_result: int) -> int:
+    graph, start, end = parse_input(puzzle)
+    x_start, y_start = start
+    x_end, y_end = end
+    best_direction = None
+    for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        try:
+            score = networkx.shortest_path_length(
+                graph, (x_start, y_start, 1, 0), (x_end, y_end, dx, dy), weight=cost
+            )
+        except (networkx.exception.NodeNotFound, networkx.exception.NetworkXNoPath):
+            print(f"no way to enter the end using direction {dx}, {dy}")
+        else:
+            if score == part_one_result:
+                best_direction = dx, dy
+    nodes = set()
+    dx, dy = best_direction
+    for path in networkx.all_shortest_paths(
+        graph, (x_start, y_start, 1, 0), (x_end, y_end, dx, dy), weight=cost
+    ):
+        nodes |= {(x, y) for (x, y, _, _) in path}
+    return len(nodes)
 
 
 def main():
@@ -139,7 +184,11 @@ def main():
     part_one_result = part_one(TEST_INPUT)
     assert part_one_result == 7036, part_one_result
     puzzle = Path("day16.txt").read_text()
-    print(part_one(puzzle))
+    part_one_real_result = part_one(puzzle)
+    print(part_one_real_result)
+    part_two_result = part_two(TEST_INPUT, part_one_result)
+    assert part_two_result == 45, part_two_result
+    print(part_two(puzzle, part_one_real_result))
 
 
 if __name__ == "__main__":
