63dbb69b2632271d16516699995a8684ef493b25
Drew Winstel <drew@hsv.beer>
2024-12-20 09:19:22 -0600

    day 20: figure out how to jump forward in the path

--- day20.py
+++ day20.py
@@ -1,4 +1,3 @@
-from itertools import permutations
 from pathlib import Path
 from typing import TypedDict
 
@@ -25,8 +24,8 @@ class CostDict(TypedDict):
     cost: int
 
 
-def display_grid(graph: networkx.DiGraph, start: tuple[int, int], end: tuple[int, int]):
-    nodes = sorted((x, y) for (x, y, _, _) in graph.nodes)
+def display_grid(graph: networkx.Graph, start: tuple[int, int], end: tuple[int, int]):
+    nodes = sorted(graph.nodes)
     (min_x, *_, max_x) = sorted(x for (x, _) in nodes)
     (min_y, *_, max_y) = sorted(y for (_, y) in nodes)
     node_set = set(nodes)
@@ -46,8 +45,8 @@ def display_grid(graph: networkx.DiGraph, start: tuple[int, int], end: tuple[int
 
 def parse_input(
     puzzle: str,
-) -> tuple[networkx.DiGraph, tuple[int, int], tuple[int, int]]:
-    graph = networkx.DiGraph()
+) -> tuple[networkx.Graph, tuple[int, int], tuple[int, int]]:
+    graph = networkx.Graph()
     lines = puzzle.splitlines()
     start = ()
     end = ()
@@ -61,19 +60,11 @@ def parse_input(
                 continue
             if char == "#":
                 continue
-            # add edges representing each possible turn
-            for (dx1, dy1), (dx2, dy2) in permutations(
-                [(1, 0), (0, 1), (-1, 0), (0, -1)], 2
-            ):
-                # unlike in day 16, turns are free
-                graph.add_edge((x, y, dx1, dy1), (x, y, dx2, dy2), cost=0)
             for x1, y1 in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
                 if x1 >= 0 and y1 >= 0:
-                    dx = x1 - x
-                    dy = y1 - y
                     try:
                         if lines[y1][x1] != "#":
-                            graph.add_edge((x, y, dx, dy), (x1, y1, dx, dy), cost=1)
+                            graph.add_edge((x, y), (x1, y1))
                     except IndexError:
                         continue
     return graph, start, end
@@ -88,27 +79,17 @@ def cost(
 
 
 def run_race(
-    graph: networkx.DiGraph, start: tuple[int, int], end: tuple[int, int]
+    graph: networkx.Graph, start: tuple[int, int], end: tuple[int, int]
 ) -> int:
     x_start, y_start = start
     x_end, y_end = end
-    best_score = 10000000000000000000000000000
-    ways_into_end = [(dx, dy) for (x, y, dx, dy) in graph.nodes if (x, y) == end]
-    for dx, dy in ways_into_end:
-        try:
-            score = networkx.shortest_path_length(
-                graph, (x_start, y_start, 1, 0), (x_end, y_end, dx, dy), weight=cost
-            )
-        except (networkx.exception.NodeNotFound, networkx.exception.NetworkXNoPath):
-            pass
-        else:
-            if score < best_score:
-                best_score = score
-    return best_score
+    score = networkx.shortest_path_length(graph, (x_start, y_start), (x_end, y_end))
+
+    return score
 
 
 def get_candidate_walls(
-    graph: networkx.DiGraph, start: tuple[int, int], end: tuple[int, int]
+    graph: networkx.Graph, start: tuple[int, int], end: tuple[int, int]
 ) -> set[tuple[int, int]]:
     x_start, y_start = start
     x_end, y_end = end
@@ -146,56 +127,79 @@ def get_candidate_walls(
     return walls
 
 
-def run_puzzle(puzzle: str, threshold: int = 100) -> tuple[int, int]:
+def manhattan_circle(x: int, y: int, radius: int):
+    s = set()
+    for i in range(radius + 1):
+        x1, y1 = (i, radius - i)
+        s |= {(x1, y1), (-x1, -y1), (x1, -y1), (-x1, y1)}
+    for d in s:
+        yield tuple(x + y for x, y in zip((x, y), d))
+
+
+def cheat_within_circle(
+    position: tuple[int, int],
+    threshold: int,
+    path: dict[tuple[int, int], int],
+    start_index: int,
+    radius: int,
+) -> bool:
+    try:
+        return path[position] - start_index - radius >= threshold
+    except KeyError:
+        return False
+
+
+def part_one(puzzle: str, threshold: int = 100) -> int:
     graph, start, end = parse_input(puzzle)
     display_grid(graph, start, end)
     base_result = run_race(graph, start, end)
     if puzzle == TEST_INPUT:
         assert base_result == 84, base_result
-    lines = puzzle.splitlines()
-    max_y = len(lines) - 1
-    max_x = len(lines[0]) - 1
-    walls = sorted(get_candidate_walls(graph=graph, start=start, end=end))
+    raw_path = networkx.shortest_path(graph, start, end)
+    path = {point: index for index, point in enumerate(raw_path)}
     saves_enough = 0
-    for x, y in walls:
-        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-            x1 = x + dx
-            y1 = y + dy
-            assert 0 <= x1 <= max_x
-            assert 0 <= y1 <= max_y
-            if lines[y1][x1] != "#":
-                # this is a possible cheat!
-                new_graph = graph.copy()
-                new_graph.add_edge((x, y, dx, dy), (x1, y1, dx, dy), cost=1)
-                # find all ways into the spot
-                for dx2, dy2 in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    x2 = x - dx
-                    y2 = y - dy
-                    if (x2, y2) == (x1, y1):
-                        continue
-                    if lines[y2][x2] != "#":
-                        # here's a way in
-                        new_graph.add_edge(
-                            (x2, y2, dx2, dy2),
-                            (x, y, dx2, dy2),
-                            cost=1,
-                        )
-                new_score = run_race(new_graph, start, end)
-                print(
-                    f"removing {x, y} heading to {x1, y1} returns {new_score}, saving {base_result - new_score}"
-                )
-                if base_result - new_score >= threshold:
+    radius = 2
+    for index, (x, y) in enumerate(raw_path):
+        for position in manhattan_circle(x, y, radius):
+            if cheat_within_circle(position, threshold, path, index, radius):
+                saves_enough += 1
+    return saves_enough
+
+
+def part_two(puzzle: str, threshold: int = 100) -> int:
+    graph, start, end = parse_input(puzzle)
+    base_result = run_race(graph, start, end)
+    if puzzle == TEST_INPUT:
+        assert base_result == 84, base_result
+    raw_path = networkx.shortest_path(graph, start, end)
+    path = {point: index for index, point in enumerate(raw_path)}
+    saves_enough = 0
+    max_radius = 20
+    for index, (x, y) in enumerate(raw_path):
+        for radius in range(2, max_radius + 1):
+            for position in manhattan_circle(x, y, radius):
+                if cheat_within_circle(position, threshold, path, index, radius):
                     saves_enough += 1
-    return saves_enough, 0
+    return saves_enough
 
 
 def main():
-    part_one_result, part_two_result = run_puzzle(TEST_INPUT, 1)
+    part_one_result = part_one(TEST_INPUT, 1)
+    # check that we get all results from the test input
     assert part_one_result == 14 + 14 + 2 + 4 + 2 + 3 + 5, part_one_result
     # assert part_two_result == 45, part_two_result
-    puzzle = Path("day16.txt").read_text()
-    part_one_real_result, part_two_real_result = run_puzzle(puzzle)
-    print(f"{part_one_real_result}\n{part_two_real_result}")
+    puzzle = Path("day20.txt").read_text()
+    part_one_real_result = part_one(puzzle)
+    print(f"{part_one_real_result}")
+    part_two_result = part_two(TEST_INPUT, 50)
+    assert (
+        part_two_result
+        == 32 + 31 + 29 + 39 + 25 + 23 + 20 + 19 + 12 + 14 + 12 + 22 + 4 + 3
+    ), (
+        part_two_result,
+        32 + 31 + 29 + 39 + 25 + 23 + 20 + 19 + 12 + 14 + 12 + 22 + 4 + 3,
+    )
+    print(part_two(puzzle))
 
 
 if __name__ == "__main__":
